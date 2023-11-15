12a851e5d479db2317f8f9d87d6382883130bab1
hesichuan <hesc@cloudinnov.com>
2023-11-15 18:08:35 +0800

    feat: 修复部署vuepress的配置，增加文档官网导航

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
+  title: "Hsc-UI-vue2",
   base: "/hsc-ui-vue2/",
   themeConfig: {
+    nav: [
+      {
+        text: "Home", link: '/'
+      }, {
+        text: "Github", link: "https://github.com/hesichuan/hsc-ui-vue2"
+      }, {
+        text: "Npm", link: "https://www.npmjs.com/package/hsc-ui-vue2"
+      }
+    ],
     sidebar: [
       '/',
       '/componentDoc/description'
